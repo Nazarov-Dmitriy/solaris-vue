3c513ac95c4685440204b2856421092add613c1f
Пользователь <olegator10205@gmail.com>
2024-04-29 10:20:19 +0300

    add changes in Trend component

--- src/components/Trend.vue
+++ src/components/Trend.vue
@@ -1,6 +1,21 @@
 <template>
   <section class="trend-container">
     <div class="trend">
+      <img
+        src="../assets/image/trend/trend-coin.png"
+        alt=""
+        class="trend__img"
+      />
+      <img
+        src="../assets/image/trend/trend-coin.png"
+        alt=""
+        class="trend__img"
+      />
+      <img
+        src="../assets/image/trend/trend-coin.png"
+        alt=""
+        class="trend__img"
+      />
       <div class="trend__content">
         <p class="trend__text p1">
           В лицее каждую неделю проводятся конкурсы по различным направлениям,
@@ -105,13 +120,35 @@
   display: flex;
   flex-direction: column;
   gap: 80px;
+  position: relative;
+}
+
+.trend__img {
+  width: 248px;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.trend__img:nth-child(1) {
+  top: 30px;
+  transform: scale(1.37) translateY(205px) translateX(35px);
+}
+.trend__img:nth-child(2) {
+  bottom: 0;
+  width: 167px;
+  transform: translateY(521px) translateX(258px) scale(1.35) rotate(9deg);
+}
+.trend__img:nth-child(3) {
+  transform: translateY(606px) translateX(113px) scale(1.35) rotate(-81deg);
+  width: 95px;
 }
 
 .trend__content {
   display: flex;
   gap: 16px;
   width: 100%;
-  padding: 110px 67px 0 60px;
+  padding: 110px 60px 0 60px;
   box-sizing: border-box;
 }
 
@@ -145,7 +182,7 @@
 
 .trend-item {
   min-height: 260px;
-  width: 100%;
+  /* width: 100%; */
   height: 100%;
   position: relative;
   grid-row: span 2;
@@ -178,11 +215,13 @@
   z-index: 1;
   left: 0;
   bottom: 0;
-  padding: 16px;
+  padding: 8px 0;
+  text-align: center;
   background: var(--roseBege);
   box-sizing: border-box;
   border: 2px solid var(--roseBege);
   cursor: pointer;
+  width: 100%;
 }
 
 .trend-item-btn:hover {
@@ -220,6 +259,7 @@
   height: 0;
 }
 
+/* адаптив для планшетов */
 @media (max-width: 900px) {
   .trend__list {
     display: flex;
@@ -257,28 +297,42 @@
     text-align: center;
   }
 
-  .trend-container-img{
+  .trend-container-img {
     height: 149px;
+  }
+
+  .trend__img:nth-child(1){
+    width: 167px;
+    transform: translate(1);
+  }
+  .trend__img:nth-child(2){
+    transform: translateX(201px) translateY(461px) scale(1.5) rotate(35deg);
+    width: 112px;
+  }
+  .trend__img:nth-child(3){
+    transform: translateX(71px) translateY(511px) scale(1.5) rotate(-54deg);
+    width: 63px;
   }
 }
 
+/* адаптив для мобилок */
 @media (max-width: 500px) {
-    .trend{
-        gap: 40px;
-    }
+  .trend {
+    gap: 40px;
+  }
   .trend__content {
     display: flex;
     align-items: center;
     flex-direction: column;
     padding: 0;
   }
-  .trend__list{
+  .trend__list {
     max-width: 300px;
     width: 100%;
     margin-right: 0;
     margin-top: 19px;
   }
-  .trend__content{
+  .trend__content {
     width: 100%;
     height: 100%;
   }
@@ -286,20 +340,32 @@
     max-width: 288px;
     margin-bottom: 16px;
   }
-  .trend-item{
-    width: 100%;
+  .trend-item {
+    /* width: 100%; */
   }
-  .trend__text{
+  .trend__text {
     margin: 0;
     padding-top: 40px;
-}
+  }
 
-.trend-container-img{
+  .trend-container-img {
     height: 91px;
-}
+  }
 
-.trend-item{
+  .trend-item {
     min-height: auto;
-}
+  }
+
+  .trend__img:nth-child(1){
+    display: none;
+  }
+  .trend__img:nth-child(2){
+    display: none;
+  }
+  .trend__img:nth-child(3){
+    top: 0;
+    left: 0;
+    transform: translateX(312px) translateY(10px) rotate(-65deg) scale(1.4);
+  }
 }
 </style>
