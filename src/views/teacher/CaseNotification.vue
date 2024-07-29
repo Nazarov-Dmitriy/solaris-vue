b670d9f7fa665191bd080fbdc09f17571e3c8fb5
Krestoprav <krestoprav@mail.ru>
2024-07-29 11:54:28 +0900

    fix

--- src/views/teacher/CaseNotification.vue
+++ src/views/teacher/CaseNotification.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="cabinet-container">
         <CabinetHeader />
-        <AddPortfolio />
+        <Notification />
         Страница Уведомления
     </div>
 </template>
 
 <script setup>
 import CabinetHeader from '@/components/cabinetStudent/CabinetHeader.vue';
-import AddPortfolio from '@/components/cabinetTeacher/case/Notification.vue';
+import Notification from '@/components/cabinetTeacher/case/Notification.vue';
 </script>
