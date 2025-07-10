fe54e9b93efdbd6cefe5723c32bf306c0630ba05
Andrw <fhdbudhh@gmail.com>
2025-07-10 18:28:02 +0300

    fix vite conf

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
